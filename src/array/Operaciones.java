4b25e6521c0f395c81a1237692e515d86aa85b30
Jhonnan Martinez <Jhonnanmartiinez@gmail.com>
2019-10-09 08:12:29 -0500

    actividad jhonnan martinez

--- src/array/Operaciones.java
+++ src/array/Operaciones.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class Operaciones {
     
     public int [] arreglo = new int [10];
-    public int opcion;
+     Scanner lectura=new Scanner(System.in);
+       
     
     //Constructor
     public Operaciones(){
-      this.mostrarMenu();
+    this.mostrarMenu();
       
         
     }
     
+    
     //Muestra el menu en pantalla
     public void mostrarMenu(){
+        
+       //limpiar pantalla 
         System.out.println(" opciones\n"
                 + "1.Llennar array\n"
                 + "2.Escribir arrays\n"
@@ -23,32 +27,53 @@ public class Operaciones {
                 + "4.Productoria\n"
                 + "5.Tamaño del array\n"
                 + "6.Salir\n"
-                + "Escoja opcion ");
-       Scanner lectura=new Scanner(System.in);
-       opcion=lectura.nextInt();
-       this.cargarOpcion(opcion);
+                + "Escoja una opcion ");
         
+      
+       int opcion = lectura.nextInt();
+       //opcion=lectura.nextInt();
+       this.cargarOpcion(opcion);
+       
     }
     //leer arreglo 
    public void leerArreglo(){
        System.out.println(" la opcion escogida es leer arreglo");
+       System.out.println(" digite numero ");
+       for (int i=0;i<10;i++){
+           
+           arreglo[i]=lectura.nextInt();
+       }
+       this.mostrarMenu();
        
    }
     //Escribe el arreglo
     public void escribirArreglo(){
         
        System.out.println(" la opcion escogida es escribir arreglo");  
+        System.out.println(" los arreglos son ");
+        for (int i=0;i<10;i++){
+            arreglo[i]=lectura.nextInt();
+
+              
+        }
+        this.mostrarMenu();
     }
     //Generar sumatoria
     public void sumatoria(){
         
        System.out.println(" la opcion escogida es sumatoria");
+        System.out.println(" la sumatoria es ");
+        
+       
+        this.mostrarMenu();
         
     }
     //Generar productoria
     public void productadoria(){
         
        System.out.println(" la opcion escogida es productadoria");
+       
+         this.mostrarMenu();
     }
     //cargar opcion
     public void cargarOpcion(int op){
@@ -56,6 +81,7 @@ public class Operaciones {
         //System.out.println("la opcion escogida es "+op);  
         
         switch(op){
+            
             case 1:
                 this.leerArreglo();
                 break;
